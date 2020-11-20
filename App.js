26c93d2feec533d8fad1ddc91d3a31e284584d24
phamtanphat <phatdroid94@gmail.com>
2020-11-20 18:47:56 +0700

    css

--- App.js
+++ App.js
@@ -1,25 +1,29 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={{flex: 1}}>
-        <Text
-          style={{
-            marginTop: 50,
-            marginLeft: 10,
-            color: 'darkseagreen',
-            fontSize: 20,
-            fontWeight: 'bold',
-            fontStyle: 'italic',
-            borderWidth: 1,
-            borderColor: 'black',
-          }}>
-          View 1
-        </Text>
+      <View style={styles.container}>
+        <Text style={styles.textStyle}>View 1</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  textStyle: {
+    marginTop: 50,
+    marginLeft: 10,
+    color: 'darkseagreen',
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+});
