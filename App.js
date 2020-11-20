9774b2c0be88fcea2a598f41e129aaffbc60ca86
phamtanphat <phatdroid94@gmail.com>
2020-11-20 19:14:51 +0700

    justifycontent and alignItems

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
         <Text style={styles.textView1}>View 1</Text>
         <Text style={styles.textView2}>View 2</Text>
         <Text style={styles.textView3}>View 3</Text>
-      </View>
+        <Text style={styles.textView4}>View 4</Text>
+      </SafeAreaView>
     );
   }
 }
@@ -17,19 +18,21 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
     backgroundColor: 'red',
   },
   textView1: {
-    flex: 1,
     backgroundColor: 'rgb(100,200,210)',
   },
   textView2: {
-    flex: 1,
     backgroundColor: 'rgb(120,130,140)',
   },
   textView3: {
-    flex: 1,
     backgroundColor: 'rgb(140,150,160)',
+  },
+  textView4: {
+    backgroundColor: 'rgb(150,160,170)',
   },
 });
