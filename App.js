558460993e4b4b9f673c8505df166ce1c132dfda
phamtanphat <phatdroid94@gmail.com>
2020-11-23 18:20:46 +0700

    position

--- App.js
+++ App.js
@@ -9,14 +9,18 @@ export default class App extends Component {
         <View
           style={{
             alignItems: 'flex-start',
-            flexWrap: 'wrap',
-            alignContent: 'center',
           }}>
           <Text style={{backgroundColor: 'red', height: 200}}>View 1</Text>
-          <Text style={{backgroundColor: 'green', height: 200}}>View 2</Text>
+          <Text
+            style={{
+              backgroundColor: 'green',
+              height: 200,
+              position: 'absolute',
+              top: 10,
+            }}>
+            View 2
+          </Text>
           <Text style={{backgroundColor: 'blue', height: 200}}>View 3</Text>
-          <Text style={{backgroundColor: 'black', height: 200}}>View 4</Text>
-          <Text style={{backgroundColor: 'yellow', height: 200}}>View 5</Text>
         </View>
       </SafeAreaView>
     );
