66ebad0947a387cea37028f1d3a5f63fec89c459
phamtanphat <phatdroid94@gmail.com>
2020-11-23 18:16:52 +0700

    flex wrap and aligncontent

--- App.js
+++ App.js
@@ -6,10 +6,18 @@ export default class App extends Component {
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <Text style={styles.textView1}>View 1</Text>
-        <Text style={styles.textView2}>View 2</Text>
-        <Text style={styles.textView3}>View 3</Text>
-        <Text style={styles.textView4}>View 4</Text>
+        <View
+          style={{
+            alignItems: 'flex-start',
+            flexWrap: 'wrap',
+            alignContent: 'center',
+          }}>
+          <Text style={{backgroundColor: 'red', height: 200}}>View 1</Text>
+          <Text style={{backgroundColor: 'green', height: 200}}>View 2</Text>
+          <Text style={{backgroundColor: 'blue', height: 200}}>View 3</Text>
+          <Text style={{backgroundColor: 'black', height: 200}}>View 4</Text>
+          <Text style={{backgroundColor: 'yellow', height: 200}}>View 5</Text>
+        </View>
       </SafeAreaView>
     );
   }
@@ -19,20 +27,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    backgroundColor: 'red',
-  },
-  textView1: {
-    backgroundColor: 'rgb(100,200,210)',
-  },
-  textView2: {
-    backgroundColor: 'rgb(120,130,140)',
-  },
-  textView3: {
-    backgroundColor: 'rgb(140,150,160)',
-  },
-  textView4: {
-    backgroundColor: 'rgb(150,160,170)',
   },
 });
