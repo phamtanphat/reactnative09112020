13078c106bfaa3936530b365cbb2365d03e242ae
phamtanphat <phatdroid94@gmail.com>
2020-11-25 18:20:49 +0700

    giao dien phan tu word

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -5,27 +5,31 @@ import AppDimensions from '../helpers/AppDimensions';
 export default class Main extends Component {
   render() {
     const words = [
-      {en: 'One', vn: 'Một'},
+      {en: 'One', vn: 'Một', isMemorized: true},
       {en: 'Two', vn: 'Hai'},
       {en: 'Three', vn: 'Ba'},
       {en: 'Four', vn: 'Bốn'},
     ];
-
     return (
       <View style={styles.container}>
         {words.map((word) => {
           return (
-            <View style={styles.groupText}>
-              <Text style={styles.txtStyleEn}>{word.en}</Text>
-              <Text style={styles.txtStyleVn}>{word.vn}</Text>
+            <View style={styles.groupItem}>
+              <View style={styles.groupElement}>
+                <Text style={styles.txtStyleEn}>{word.en}</Text>
+                <Text style={styles.txtStyleVn}>{word.vn}</Text>
+              </View>
+              <View style={styles.groupElement}>
+                <TouchableOpacity style={styles.touchMemorized}>
+                  <Text style={styles.textMemorized}>Memorized</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.touchRemove}>
+                  <Text style={styles.textRemove}>Remove</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           );
         })}
-        {/* <TouchableOpacity
-          ref={(refs) => (this.touchable = refs)}
-          style={{backgroundColor: 'gray'}}>
-          <Text>TouchableOpacity</Text>
-        </TouchableOpacity> */}
       </View>
     );
   }
@@ -35,18 +39,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  groupText: {
+  groupItem: {
+    margin: 5,
+    height: 150,
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    borderRadius: 10,
+    backgroundColor: 'silver',
+  },
+  groupElement: {
+    width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-evenly',
   },
   txtStyleEn: {
     color: 'green',
-    fontSize: AppDimensions.getWidth() / 20,
+    fontSize: AppDimensions.getWidth() / 15,
   },
   txtStyleVn: {
     color: 'red',
+    fontSize: AppDimensions.getWidth() / 15,
+  },
+  touchRemove: {
+    backgroundColor: 'khaki',
+    padding: 10,
+    borderRadius: 5,
+  },
+  touchMemorized: {
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 5,
+  },
+  textMemorized: {
     fontSize: AppDimensions.getWidth() / 20,
   },
+  textRemove: {
+    fontSize: AppDimensions.getWidth() / 20,
+  }
 });
