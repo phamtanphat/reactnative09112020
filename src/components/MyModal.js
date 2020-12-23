4d49f572685c54b0985e41e68cb9f955f9a919b2
phamtanphat <phatdroid94@gmail.com>
2020-12-23 18:29:45 +0700

    modal

--- src/components/MyModal.js
+++ src/components/MyModal.js
@@ -1,17 +1,24 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {Text, View, TouchableOpacity} from 'react-native';
+import {Text, View, TouchableOpacity, Modal} from 'react-native';
 
 export default class MyModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalVisible: false,
+    };
+  }
   render() {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Modal visible={this.state.modalVisible}>
+          <View style={{flex: 1}}>
+            <Text>Hộp thoại</Text>
+          </View>
+        </Modal>
         <TouchableOpacity style={{backgroundColor: 'green', padding: 10}}>
           <Text style={{color: 'white'}}>Open modal</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{backgroundColor: 'red', padding: 10, marginTop: 30}}>
-          <Text style={{color: 'white'}}>Close modal</Text>
         </TouchableOpacity>
       </View>
     );
