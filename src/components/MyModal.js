db33180e8a0ae489980a03c015a07255d71b6e2e
phamtanphat <phatdroid94@gmail.com>
2020-12-23 18:46:53 +0700

    ui modal

--- src/components/MyModal.js
+++ src/components/MyModal.js
@@ -9,15 +9,44 @@ export default class MyModal extends Component {
       modalVisible: false,
     };
   }
+  toggleModal = () => {
+    this.setState({modalVisible: !this.state.modalVisible});
+  };
   render() {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Modal visible={this.state.modalVisible}>
-          <View style={{flex: 1}}>
-            <Text>Hộp thoại</Text>
+        <Modal transparent={true} visible={this.state.modalVisible}>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '100%',
+              height: '100%',
+            }}>
+            <View
+              style={{
+                borderRadius: 5,
+                borderBottomColor: 'blue',
+                borderColor: 'black',
+                width: 300,
+                height: 300,
+                backgroundColor: 'whitesmoke',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Text>Hộp thoại</Text>
+              <TouchableOpacity
+                onPress={this.toggleModal}
+                style={{backgroundColor: 'green', padding: 10}}>
+                <Text style={{color: 'white'}}>Close modal</Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </Modal>
-        <TouchableOpacity style={{backgroundColor: 'green', padding: 10}}>
+
+        <TouchableOpacity
+          onPress={this.toggleModal}
+          style={{backgroundColor: 'green', padding: 10}}>
           <Text style={{color: 'white'}}>Open modal</Text>
         </TouchableOpacity>
       </View>
